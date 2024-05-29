698089d3330105fdf6d8554099de1f0a0e932e22
Exerea <ryu.vs.lime.and.takumi@ezweb.ne.jp>
2024-05-29 23:22:37 +0900

    add

---
+++ 20240529/e.py
@@ -0,0 +1,13 @@
+def main():
+    N = int(input())
+
+    # ab  = []
+    # for i in range(N):
+    #     a,b = int(int,input().split())
+    #     ab.append([a,b])
+
+    print()
+
+
+if __name__ == "__main__":
+    main()
